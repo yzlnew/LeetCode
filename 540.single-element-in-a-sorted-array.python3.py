cebb0007d8d3b9947617ff003ea1c9636d31a684
Ye Zhiling <yzlnew@gmail.com>
2018-05-14 23:44:09 -0500

    update 540.single-element-in-a-sorted-array.python3.py

---
+++ 540.single-element-in-a-sorted-array.python3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        low, high = 0, len(nums) - 1
+
+        while low < high:
+            mid = (low + high) // 2
+
+            if nums[mid] == nums[mid ^ 1]:
+                low = mid + 1
+            else:
+                high = mid
+
+        return nums[low]
