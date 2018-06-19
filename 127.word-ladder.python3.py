e9981d49daacdff6bf32796b4bf402bb55ca3f56
Ye Zhiling <yzlnew@gmail.com>
2018-06-18 22:04:18 -0500

    update 127.word-ladder.python3.py

--- 127.word-ladder.python3.py
+++ 127.word-ladder.python3.py
@@ -17,7 +17,8 @@ class Solution:
 
         while s1 and s2:
             step = step + 1
-            s1, s2 = s2, s1
+            if len(s1) > len(s2):
+                s1, s2 = s2, s1
             s = set()
 
             for word in s1:
