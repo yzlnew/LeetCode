3623b18b1d77b70d26d5728e3e322022f49e942d
Ye Zhiling <yzlnew@gmail.com>
2018-07-31 11:27:06 +0800

    update 130.surrounded-regions.python3.py

--- 130.surrounded-regions.python3.py
+++ 130.surrounded-regions.python3.py
@@ -4,18 +4,22 @@ class Solution:
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
-        if not board: return
-        m, n = len(board), len(board[0])
-        boundary = [loc for i in range(m) for loc in (
-            (i, 0), (i, n - 1))] + [loc for i in range(n) for loc in ((0, i), (m - 1, i))]
 
+        if not board:
+            return
+        m, n = len(board), len(board[0])
+        # Get boundary
+        boundary = [loc for i in range(m) for loc in ((i, 0), (i, n - 1))] + [
+            loc for i in range(n) for loc in ((0, i), (m - 1, i))
+        ]
+        # Keep adding surrounding to boundary and change mark
         while boundary:
             x, y = boundary.pop()
 
             if x in range(m) and y in range(n) and board[x][y] == 'O':
                 boundary += [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
                 board[x][y] = 'M'
-
+        # Reset mark
         for row in board:
             for col, val in enumerate(row):
                 if val == 'M':
